a8c55a9c968286a6933bc64eb9403ca87f5055f3
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2023-08-11 15:58:41 +0300

    Use tab to toggle echo password

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ extern crate alloc;
 
 use alloc::string::String;
 use core::char;
+use core::iter::repeat;
 
 use uefi::entry;
 use uefi::proto::console::text::Key;
+use uefi::proto::console::text::ScanCode;
 use uefi::table::Boot;
 use uefi::table::SystemTable;
 use uefi::Handle;
@@ -33,8 +35,13 @@ fn read_password(system_table: &mut SystemTable<Boot>) -> Result<String, String>
     print!("[guardianboot] Please enter your password: ");
     let mut password = String::new();
 
+    // remember cursor visibility to set it back to the default after reading
+    // password.
     let cursor_visible = system_table.stdout().cursor_visible();
     system_table.stdout().enable_cursor(true).unwrap();
+
+    // whether to echo asterisks (*) as the password is being typed out.
+    let mut echo = true;
 
     loop {
         // Pause until a keyboard event occurs.
@@ -46,23 +53,44 @@ fn read_password(system_table: &mut SystemTable<Boot>) -> Result<String, String>
         let key = system_table.stdin().read_key().unwrap();
         match key {
             Some(Key::Printable(ch)) => match char::from(ch) {
-                // enter key is pressed. this means the password is enetered.
+                // enter key. this means the password is enetered.
                 '\r' => {
                     println!();
                     break;
                 }
+                // tab key. toggle echo.
+                '\t' => {
+                    echo = !echo;
+                    if echo {
+                        let _ = system_table.stdout().enable_cursor(true);
+                        let asterisks = repeat("*")
+                            .take(password.chars().count())
+                            .collect::<String>();
+                        print!("{}", asterisks);
+                    } else {
+                        let _ = system_table.stdout().enable_cursor(false);
+                        for _ in 0..password.chars().count() {
+                            print!("\x08 \x08");
+                        }
+                    }
+                }
                 // backspace key. remove the last enetered character.
                 '\x08' => {
-                    if password.pop().is_some() {
-                        // visually remove last character
+                    if password.pop().is_some() && echo {
+                        // visually remove last character if echo is enabled
                         print!("\x08 \x08");
                     }
                 }
                 ch => {
                     password.push(ch);
-                    print!("*");
+                    if echo {
+                        print!("*");
+                    }
                 }
             },
+            Some(Key::Special(ScanCode::FUNCTION_2)) => {
+                // show password
+            }
             _ => {}
         }
     }
