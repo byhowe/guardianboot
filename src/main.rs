97bb43e269923b8953621dde111f5e0beccbc1ff
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2023-08-20 03:24:04 +0300

    OMG it works finally

--- src/main.rs
+++ src/main.rs
@@ -4,48 +4,173 @@
 extern crate alloc;
 
 use alloc::string::String;
-use core::char;
-use core::iter::repeat;
+use alloc::string::ToString;
+use alloc::vec;
+use alloc::vec::Vec;
+use core::ops::Deref;
+use core::ops::DerefMut;
+use core::ptr;
+use core::slice;
 
+use uefi::cstr16;
 use uefi::entry;
+use uefi::fs::FileSystem;
+use uefi::fs::Path;
 use uefi::proto::console::text::Key;
-use uefi::proto::console::text::ScanCode;
+use uefi::proto::device_path::text::AllowShortcuts;
+use uefi::proto::device_path::text::DisplayOnly;
+use uefi::proto::device_path::DevicePath;
+use uefi::proto::loaded_image::LoadedImage;
+use uefi::proto::media::file::File;
+use uefi::proto::media::file::FileAttribute;
+use uefi::proto::media::file::FileInfo;
+use uefi::proto::media::file::FileMode;
+use uefi::proto::media::fs::SimpleFileSystem;
+use uefi::proto::ProtocolPointer;
+use uefi::table::boot::AllocateType;
+use uefi::table::boot::LoadImageSource;
+use uefi::table::boot::MemoryType;
+use uefi::table::boot::ScopedProtocol;
 use uefi::table::Boot;
 use uefi::table::SystemTable;
+use uefi::CStr16;
+use uefi::CString16;
+use uefi::Char16;
 use uefi::Handle;
 use uefi::Status;
-use uefi_services::print;
 use uefi_services::println;
+
 mod password_reader;
 
 #[entry]
-fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status
+fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status
 {
     uefi_services::init(&mut system_table).unwrap();
-    println!("Hello World!");
-    loop {
-        let password = read_password(&mut system_table).unwrap();
-        println!("The password entered is: {:?}", password);
-    }
+
+    println!("Loading the UKI into memory");
+    let uki_data = load_file("vmlinuz-linux.unsigned.efi").unwrap();
+    println!("Press ENTER to launch the UKI");
+    await_enter();
+    start_image(uki_data);
+
     Status::SUCCESS
 }
 
-/// Read password from stdin.
-fn read_password(system_table: &mut SystemTable<Boot>) -> Result<String, String>
+/// Opens the device path protocol and returns a scoped protocol for the device
+/// path.
+///
+/// # Returns
+///
+/// A `ScopedProtocol` for the `DevicePath`.
+fn open_device_path_protocol<'a>() -> ScopedProtocol<'a, DevicePath>
 {
-    print!("[guardianboot] Please enter your password: ");
-    let mut password = String::new();
+    let system_table = unsafe { uefi_services::system_table().as_mut() };
+    let image_handle = system_table.boot_services().image_handle();
+    let loaded_image = system_table
+        .boot_services()
+        .open_protocol_exclusive::<LoadedImage>(image_handle)
+        .unwrap();
+    let device_handle = loaded_image.device();
+    let device_path = system_table
+        .boot_services()
+        .open_protocol_exclusive::<DevicePath>(device_handle)
+        .unwrap();
+    device_path
+}
 
-    // remember cursor visibility to set it back to the default after reading
-    // password.
-    let cursor_visible = system_table.stdout().cursor_visible();
-    system_table.stdout().enable_cursor(true).unwrap();
+/// Locates and opens a protocol of type `P`.
+///
+/// # Type Parameters
+///
+/// * `P`: The type of protocol pointer to locate and open.
+///
+/// # Returns
+///
+/// A `ScopedProtocol` that wraps the opened protocol handle.
+fn locate_and_open_protocol<'a, P: ProtocolPointer>() -> ScopedProtocol<'a, P>
+{
+    let system_table = unsafe { uefi_services::system_table().as_mut() };
+    let device_path = open_device_path_protocol();
+    let mut device_path = device_path.deref();
+    let fs_handle = system_table
+        .boot_services()
+        .locate_device_path::<P>(&mut device_path)
+        .unwrap();
+    let opened_handle = system_table
+        .boot_services()
+        .open_protocol_exclusive(fs_handle)
+        .unwrap();
+    opened_handle
+}
 
-    // whether to echo asterisks (*) as the password is being typed out.
-    let mut echo = true;
+/// Loads a file from the given path and returns a mutable reference to its
+/// contents.
+///
+/// # Arguments
+///
+/// * `path` - A string slice that holds the path to the file to be loaded.
+///
+/// # Returns
+///
+/// An `Option` that contains a mutable reference to the contents of the loaded
+/// file if the file was successfully loaded, or `None` otherwise.
+fn load_file(path: &str) -> Option<&'static mut [u8]>
+{
+    let system_table = unsafe { uefi_services::system_table().as_mut() };
+    let mut fs = locate_and_open_protocol::<SimpleFileSystem>();
+    let mut root = fs.open_volume().unwrap();
+    let path = CString16::try_from(path).unwrap();
+    let file_handle = root
+        .open(&path, FileMode::Read, FileAttribute::empty())
+        .ok()?;
+    let mut file = file_handle.into_regular_file()?;
+    let file_info = file.get_boxed_info::<FileInfo>().unwrap();
+    let file_ptr = system_table
+        .boot_services()
+        .allocate_pages(
+            AllocateType::AnyPages,
+            MemoryType::LOADER_DATA,
+            ((file_info.file_size() as usize - 1) / 4096) + 1,
+        )
+        .unwrap() as *mut u8;
+    unsafe { ptr::write_bytes(file_ptr, 0, file_info.file_size() as usize) };
+    let file_buf = unsafe { slice::from_raw_parts_mut(file_ptr, file_info.file_size() as usize) };
+    file.read(file_buf).unwrap();
 
+    Some(file_buf)
+}
+
+/// Loads and starts an UEFI image from a buffer.
+///
+/// # Arguments
+///
+/// * `uki` - A slice of bytes containing the Unified Kernel Image to load.
+fn start_image(uki: &[u8])
+{
+    let system_table = unsafe { uefi_services::system_table().as_mut() };
+    let image_handle = system_table.boot_services().image_handle();
+    let device_path = open_device_path_protocol();
+    let loaded_image = system_table
+        .boot_services()
+        .load_image(
+            image_handle,
+            LoadImageSource::FromBuffer {
+                buffer: uki,
+                file_path: Some(device_path.get().unwrap()),
+            },
+        )
+        .unwrap();
+    system_table
+        .boot_services()
+        .start_image(loaded_image)
+        .unwrap();
+}
+
+/// Waits for the user to press the Enter key before returning.
+fn await_enter()
+{
+    let system_table = unsafe { uefi_services::system_table().as_mut() };
     loop {
-        // Pause until a keyboard event occurs.
         let mut events = unsafe { [system_table.stdin().wait_for_key_event().unsafe_clone()] };
         system_table
             .boot_services()
@@ -53,49 +178,8 @@ fn read_password(system_table: &mut SystemTable<Boot>) -> Result<String, String>
             .unwrap();
         let key = system_table.stdin().read_key().unwrap();
         match key {
-            Some(Key::Printable(ch)) => match char::from(ch) {
-                // enter key. this means the password is enetered.
-                '\r' => {
-                    println!();
-                    break;
-                }
-                // tab key. toggle echo.
-                '\t' => {
-                    echo = !echo;
-                    if echo {
-                        let _ = system_table.stdout().enable_cursor(true);
-                        let asterisks = repeat("*")
-                            .take(password.chars().count())
-                            .collect::<String>();
-                        print!("{}", asterisks);
-                    } else {
-                        let _ = system_table.stdout().enable_cursor(false);
-                        for _ in 0..password.chars().count() {
-                            print!("\x08 \x08");
-                        }
-                    }
-                }
-                // backspace key. remove the last enetered character.
-                '\x08' => {
-                    if password.pop().is_some() && echo {
-                        // visually remove last character if echo is enabled
-                        print!("\x08 \x08");
-                    }
-                }
-                ch => {
-                    password.push(ch);
-                    if echo {
-                        print!("*");
-                    }
-                }
-            },
-            Some(Key::Special(ScanCode::FUNCTION_2)) => {
-                // show password
-            }
+            Some(Key::Printable(ch)) if char::from(ch) == '\r' => return,
             _ => {}
         }
     }
-
-    system_table.stdout().enable_cursor(cursor_visible).unwrap();
-    Ok(password)
 }
